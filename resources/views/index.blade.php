b27fa3e49e358f17dcc8177196bc77a8c8dad51c
asami-12 <asami-12@example.com>
2026-02-25 10:39:36 +0000

    formテスト用のコードを書いた

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 {{-- メインコンテンツ --}}
 @section('contents')
     <h1>ログイン</h1>
-    <form action="./login" method="post">
+    <form action="/login" method="post">
         email：<input type="text" name="email"><br>
         パスワード：<input type="password" name="password"><br>
         <button>ログインする</button>
