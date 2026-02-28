80ac4c2859024cfe1b667f776d2b58a650016bba
asami-12 <asami-12@example.com>
2026-02-28 05:26:13 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,11 +17,25 @@ class TaskController extends Controller
      */
     public function list()
     {
+        // 1Page辺りの表示アイテム数を設定
+        $per_page = 20;
+
         // 一覧の取得
-        $list = TaskModel::where('user_id', Auth::id())->orderBy('priority', 'DESC')->orderBy('period')->orderBy('created_at')->get();
-        $sql = TaskModel::where('user_id', Auth::id())->orderBy('priority', 'DESC')->orderBy('period')->orderBy('created_at')->toSql();
+        $list = TaskModel::where('user_id', Auth::id())
+                         ->orderBy('priority', 'DESC')
+                         ->orderBy('period')
+                         ->orderBy('created_at')
+                         ->paginate($per_page);
+                        //  ->get();
+        /*
+        $sql = TaskModel::where('user_id', Auth::id())
+                        ->orderBy('priority', 'DESC')
+                        ->orderBy('period')
+                        ->orderBy('created_at')
+                        ->toSql();
         // echo "<pre>\n"; var_dump($sql, $list); exit;
         // var_dump($sql);
+        */
 
         return view('task.list', ['list' => $list]);
     }
