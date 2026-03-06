66372366d8096af48dbbf06a3909edfaccf3e9bb
asami-12 <asami-12@example.com>
2026-03-06 14:08:16 +0000

    完了タスク一覧を追加

---
+++ app/Http/Controllers/CompletedTaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\TaskRegisterPostRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task as TaskModel;
+use Illuminate\Support\Facades\DB;
+use App\Models\CompletedTask as CompletedTaskModel;
+
+
+class CompletedTaskController extends Controller
+{
+    protected function getListBuilder()
+    {
+        return CompletedTaskModel::where('user_id', Auth::id())
+                ->orderBy('priority', 'DESC')
+                ->orderBy('period')
+                ->orderBy('created_at');
+    }
+
+    public function list()
+    {
+        $per_page = 20;
+        $list = $this->getListBuilder()
+                     ->paginate($per_page);
+        return view('task.completed_list', ['list' => $list]);
+    }
+}
